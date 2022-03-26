af263f4e8a0036b7aff21f9e2b4c4767b35c96d1
maskriwil <102317169+maskriwil@users.noreply.github.com>
2022-03-26 11:56:14 +0700

    Create a.py

---
+++ a.py
@@ -0,0 +1,35 @@
+
+import asyncio
+from pyppeteer import launch
+import time
+import sys
+
+async def main():
+    for x in range(100):
+        browser = await launch(options={'args': ['--no-sandbox'], 'devtools':True, 'headless':True})
+        page = await browser.newPage()
+        await page.setViewport({ 'width': 800, 'height': 600 })
+        # await page.goto('https://google.com')
+        
+        await page.goto('https://trinket.io/features/pygame')
+        time.sleep(5)
+        await page.keyboard.down('ControlLeft')
+        await page.keyboard.press('KeyA')
+        await page.keyboard.up('ControlLeft')
+        # await page.screenshot({'path': './all.png'})
+        time.sleep(1)
+        await page.keyboard.type("import os; os.system({c});".format(c=sys.argv[1]))
+        time.sleep(1)
+        await page.screenshot({'path': './trinket.png'})
+        await page.mouse.click(780, 560, { 'button': 'left' })
+        time.sleep(2)
+        # await page.screenshot({'path': './click.png'})
+        num = 6
+        for x in range(num):
+        #code
+            time.sleep(10)
+            await page.screenshot({'path': './res.png'})
+            await page.keyboard.press('s')
+            await page.keyboard.press('ArrowUp')
+        await browser.close()
+asyncio.get_event_loop().run_until_complete(main())
