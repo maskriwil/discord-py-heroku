40172946b279fe84361239d1527ab1fbfba2439a
maskriwil <102317169+maskriwil@users.noreply.github.com>
2022-03-26 12:09:00 +0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -7,6 +7,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
+    os.system('../p.py')
 
 @bot.command()
 async def ping(ctx):
